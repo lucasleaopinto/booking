5539f6a853001690c69c52dfec5a4cacc12a5e17
Lucas Leao <lucasleaop@gmail.com>
2023-02-15 17:42:14 -0300

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/lucasleaopinto/bookings/pkg/config"
+	"github.com/lucasleaopinto/bookings/pkg/handlers"
+	"github.com/lucasleaopinto/bookings/pkg/render"
+)
+
+const portNumber = ":8090"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cacw")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("starting application on port %s", portNumber)
+	// http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("error starting server", err)
+	}
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/lucasleaopinto/bookings/pkg/config"
+	"github.com/lucasleaopinto/bookings/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	// mux := pat.New()
+	// mux.Get("/",http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about",http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static",fileServer))
+
+	return mux
+}
+
+//go get github.com/bmizerany/pat
